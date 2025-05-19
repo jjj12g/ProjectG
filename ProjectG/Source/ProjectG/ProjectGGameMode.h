3dda35571d594d76cbd8142f557a1458e5b56320
Jinwon Hong <159858734+jjj12g@users.noreply.github.com>
2025-05-19 19:02:26 +0900

    Project Start
    기본구성 + 깃 이그노어

---
+++ ProjectG/Source/ProjectG/ProjectGGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectGGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectGGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectGGameMode();
+};
+
+
+
